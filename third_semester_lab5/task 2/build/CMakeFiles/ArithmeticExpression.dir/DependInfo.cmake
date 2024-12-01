7254c30930f032c5e7ca7c6f2a4fa8be371d26d9
zepif <zellti152@gmail.com>
2024-12-01 21:44:24 +0100

    add lab5 task 2

---
+++ third_semester_lab5/task 2/build/CMakeFiles/ArithmeticExpression.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/acer/task 2/src/ArithmeticExpression.cpp" "CMakeFiles/ArithmeticExpression.dir/src/ArithmeticExpression.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/ArithmeticExpression.cpp.o.d"
+  "/home/acer/task 2/src/Comparers/DiagonalProductComparer.cpp" "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductComparer.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductComparer.cpp.o.d"
+  "/home/acer/task 2/src/Comparers/DiagonalProductThenNextComparer.cpp" "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductThenNextComparer.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductThenNextComparer.cpp.o.d"
+  "/home/acer/task 2/src/Helpers.cpp" "CMakeFiles/ArithmeticExpression.dir/src/Helpers.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/Helpers.cpp.o.d"
+  "/home/acer/task 2/src/Loader.cpp" "CMakeFiles/ArithmeticExpression.dir/src/Loader.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/Loader.cpp.o.d"
+  "/home/acer/task 2/src/Matrix.cpp" "CMakeFiles/ArithmeticExpression.dir/src/Matrix.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/Matrix.cpp.o.d"
+  "/home/acer/task 2/src/Node.cpp" "CMakeFiles/ArithmeticExpression.dir/src/Node.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/Node.cpp.o.d"
+  "/home/acer/task 2/src/VectorAnalog.cpp" "CMakeFiles/ArithmeticExpression.dir/src/VectorAnalog.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/VectorAnalog.cpp.o.d"
+  "/home/acer/task 2/src/main.cpp" "CMakeFiles/ArithmeticExpression.dir/src/main.cpp.o" "gcc" "CMakeFiles/ArithmeticExpression.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ third_semester_lab5/task 2/build/CMakeFiles/ArithmeticExpression.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "ArithmeticExpression"
+  "ArithmeticExpression.pdb"
+  "CMakeFiles/ArithmeticExpression.dir/src/ArithmeticExpression.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/ArithmeticExpression.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductComparer.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductComparer.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductThenNextComparer.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/Comparers/DiagonalProductThenNextComparer.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/Helpers.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/Helpers.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/Loader.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/Loader.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/Matrix.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/Matrix.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/Node.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/Node.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/VectorAnalog.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/VectorAnalog.cpp.o.d"
+  "CMakeFiles/ArithmeticExpression.dir/src/main.cpp.o"
+  "CMakeFiles/ArithmeticExpression.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArithmeticExpression.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
